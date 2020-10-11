0b13ab6c6d460b435c0677d025bf7f7959a2a8a0
BenLoewen <BenjaminJLoewen@gmail.com>
2020-10-11 11:23:54 -0700

    Update setup.sh

--- tool-kit/linux/setup-tool/setup.sh
+++ tool-kit/linux/setup-tool/setup.sh
@@ -3,14 +3,5 @@ pip install virtualenv
 yarn global add create-react-app react-scripts
 python3 -m venv env
 source env/bin/activate
-npm install react-dev-utils
-npm install react-bootstrap
-npm install jsx-to-string
-npm install jquery
-npm install html5-file-selector
-npm install react-dropzone
-npm install styled-components
-npm install use-force-update
-npm install bootstrap
-npm install fs-extra
+npm install
 pip install flask
