66393250713f0090fb351d90ae1d5c7950f57457
BenLoewen <BenjaminJLoewen@gmail.com>
2020-10-24 16:42:14 -0700

    Delete screenCap.sh

--- screenCap/screenCap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-now=$(date +"%R")
-window=$(xdotool getwindowfocus getwindowname)
-name="$now-$window"
-scrot -u /home/pi/Desktop/screenCap/captures/"$name".jpg
-node /home/pi/Desktop/screenCap/sendImage.js "$name"
