079d188bcedcbc3b6f387cb98c8492882b77e15a
BenLoewen <BenjaminJLoewen@gmail.com>
2020-12-04 19:17:40 -0800

    Imported JQuery for Ajax

--- shortcuts/screenCap/sendImage.js
+++ shortcuts/screenCap/sendImage.js
@@ -1,18 +1,20 @@
+import $ from "jquery";
+
 process.argv.forEach(function(val,index,array){
   if(index==2){
-    toSend = {"url":val}
+    toSend = {"name":val}
     $.ajax({
       type: "POST",
       url: "/screen_cap",
       dataType: "json",
       contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(toSend)
+      data: JSON.stringify(toSend),s
       success: function(result){
         console.log("success")
       },
       error: function(request, status, error){
         console.log("Failure. Server is probably not running")
-      }
+      }s
     }
   }
 });
