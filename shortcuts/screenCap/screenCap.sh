efbedcb82403a7208852e77dd7cab4e112a9c33a
BenLoewen <BenjaminJLoewen@gmail.com>
2020-12-11 12:42:13 -0800

    Updated to Linux Files

--- shortcuts/screenCap/screenCap.sh
+++ shortcuts/screenCap/screenCap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 now=$(date)
 name="${now// /_}"
-scrot -s /home/bloewen/elog/elog-test/shortcuts/screenCap/captures/"$name".jpg
+scrot -s /home/bloewen/elog/lyncean-elog/shortcuts/screenCap/captures/"$name".jpg
 node sendImage.js "$name"
--- shortcuts/screenCap/sendImage.js
+++ shortcuts/screenCap/sendImage.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 process.argv.forEach(function(val,index,array){
   if(index==2){
-    axios.get("http://localhost:4040/"+val)
+    axios.post("http://localhost:4040/upload_screen_cap/"+val)
       .then(response => {
 	console.log("success");
       })
