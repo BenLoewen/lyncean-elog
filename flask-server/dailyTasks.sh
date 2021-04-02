0c7e4a6891c5863135d9852d7d53cdb04287b72b
BenLoewen <BenjaminJLoewen@gmail.com>
2021-04-02 14:09:20 -0700

    Delete dailyTasks.sh

--- flask-server/dailyTasks.sh
+++
@@ -1,4 +0,0 @@
-source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate pydm-environment
-python /home/bloewen/elog/lyncean-elog/flask-server/autojobs.py
-python /home/bloewen/elog/lyncean-elog/flask-server/generate-reports.py
