babaeb298ccc196e7d37b7c66d1b762fe681f389
BenLoewen <BenjaminJLoewen@gmail.com>
2021-04-02 14:10:45 -0700

    Delete shortcuts/newswriter directory

--- shortcuts/newswriter/selectWindow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-now=$(date)
-name="${now// /_}"
-scrot -u /home/bloewen/elog/lyncean-elog/shortcuts/screenCap/captures/"$name".jpg
-python /home/bloewen/elog/lyncean-elog/shortcuts/newswriter/newswriter2.py "$name"
